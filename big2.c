130101bb6529fa8299697c00e333856ee4161442
Cloud User <ec2-user@ip-172-31-20-100.eu-north-1.compute.internal>
2023-12-17 17:29:46 +0000

    done

---
+++ big2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void biggest ()
+{
+    int num1, num2, temp;
+//ABC
+    num1=20,num2=30;
+    if(num1 == num2)
+        printf("Both are Equal\n");
+    else{
+        temp = num1 > num2? num1 : num2;
+        printf("%d is largest",temp);
+    }
+
+ // return 0;
+}
+
